ae997aa6cb49c2eefeae26c855a89f7183249782
letanatol <letanatol@gmail.com>
2023-12-21 00:28:08 +0100

    feat: add solution task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
+ * Создать повторяющуюся строку на основе заданных параметров
  *  
  * @param {String} str string to repeat
  * @param {Object} options options object 
@@ -10,14 +11,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * @example
  * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
- * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
+ * repeater('STRING', 
+ * { repeatTimes: 3, 
+ * separator: '**', 
+ * addition: 'PLUS', 
+ * additionRepeatTimes: 3, 
+ * additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const customOptions = {
+    repeatTimes: 1,
+    separator: '+',
+    addition: '',
+    additionRepeatTimes: 1,
+    additionSeparator: '|'
+  }
+  
+  options = { ...customOptions, ...options };
+  options.addition = String(options.addition);
+
+  let smallItem = [];
+  let bigItem = [];
+
+  // if (!options.hasOwnProperty('addition') && options.repeatTimes === 1) {
+  //   return str;
+  // }
+
+  if ((options.addition === '') && options.repeatTimes === 1) {
+    return str;
+  }
+
+  if ((options.addition === '') && options.repeatTimes > 1) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      bigItem.push(str);
+      console.log(bigItem);
+    }
+    return bigItem.join(options.separator);
+  }
+
+  for (let i = 0; i < options.additionRepeatTimes; i++) {
+    smallItem.push(options.addition);
+  }
+
+  let additionItemString = `${str}${smallItem.join(options.additionSeparator)}`;
+
+  for (let i = 0; i < options.repeatTimes; i++) {
+    bigItem.push(additionItemString);
+  }
+
+  return bigItem.join(options.separator);
 }
 
 module.exports = {
