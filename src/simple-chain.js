7ca84d0ffdbf86da3358eaa45a2e145e2aecdba6
letanatol <letanatol@gmail.com>
2023-12-21 06:24:02 +0100

    feat: add solution task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -3,27 +3,36 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement chainMaker object according to task description
  * 
+ * Реализация объекта chainMaker в соответствии с описанием задачи
+ * 
  */
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.array.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.array.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (!this.array[position - 1]) {
+      // (position < 1 || position > this.array.length || isNaN(position) || !Number.isInteger(position)
+      this.array = [];
+      throw new Error("You can\'t remove incorrect link!");
+    } else {
+      this.array.splice(position - 1, 1)
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let chain = this.array.join('~~');
+    this.array = [];
+    return chain;
   }
 };
 
