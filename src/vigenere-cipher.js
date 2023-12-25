681d034374a6cc0a5b4733bd4380244e97944737
letanatol <letanatol@gmail.com>
2023-12-25 06:07:44 +0100

    feat: add solution task VigenereCipheringMachine

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -4,6 +4,9 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
  * 
+ * Реализация класса VigenereCipheringMachine, который позволяет нам создавать
+ * машины прямого и обратного шифрования в соответствии с описанием задачи
+ * 
  * @example
  * 
  * const directMachine = new VigenereCipheringMachine();
@@ -18,16 +21,103 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
+ * Например, если открытый текст attacking tonightи ключ OCULORHINOLARINGOLOGY, то
+первая буква a открытого текста сдвигается на 14 позиций в алфавите (поскольку первая буква O ключа — это 14-я буква алфавита, считая с 0), что дает o;
+вторая буква t сдвигается на 2 (поскольку вторая буква C ключа означает 2), что дает v;
+третья буква t сдвигается на 20 ( U), что дает n, с переносом;
+и так далее; выдавая сообщение ovnlqbpvt hznzouz. Если получатель сообщения знает ключ, он может восстановить открытый текст, обратив этот процесс.
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(condition = true) {
+    this.condition = condition;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, cipher) {
+    if (!str || !cipher) throw new Error("Incorrect arguments!");
+
+    let cipherUpper = cipher.toUpperCase();
+    while (str.length > cipherUpper.length) {
+      cipherUpper += cipherUpper;
+    }
+
+    let arrayCipher = [];
+    for (let i = 0; i < cipherUpper.length; i++) {
+      arrayCipher.push(cipherUpper[i]);
+    }
+
+    let strUpper = str.toUpperCase();
+    const arrayStr = [];
+    for (let i = 0; i < strUpper.length; i++) {
+      arrayStr.push(strUpper[i]);
+    }
+
+    const arrayResult = [];
+
+    for (let i = 0; i < arrayStr.length; i++) {
+      if (arrayStr[i].charCodeAt() >= 65 && arrayStr[i].charCodeAt() <= 90) {
+        let foo = ((arrayCipher[0].charCodeAt()) - 65) + arrayStr[i].charCodeAt();
+        if (foo > 90) {
+          foo = foo - 90 + 64;
+        }
+        arrayResult.push(String.fromCharCode(foo));
+        arrayCipher.shift();
+      } else {
+        arrayResult.push(strUpper[i]);
+      }
+    }
+
+    if (this.condition) {
+      return arrayResult.join('');
+    } else {
+      return arrayResult.reverse().join('');
+    }
   }
+
+
+  decrypt(str, cipher) {
+    if (!str || !cipher) throw new Error("Incorrect arguments!");
+
+    let cipherUpper = cipher.toUpperCase();
+    while (str.length > cipherUpper.length) {
+      cipherUpper += cipherUpper;
+    }
+
+    let arrayCipher = [];
+    for (let i = 0; i < cipherUpper.length; i++) {
+      arrayCipher.push(cipherUpper[i]);
+    }
+
+    let strUpper = str.toUpperCase();
+    
+    const arrayStr = [];
+
+    for (let i = 0; i < strUpper.length; i++) {
+      arrayStr.push(strUpper[i]);
+    }
+
+    const arrayResult = [];
+
+    for (let i = 0; i < arrayStr.length; i++) {
+      if (arrayStr[i].charCodeAt() >= 65 && arrayStr[i].charCodeAt() <= 90) {
+        let foo = (arrayStr[i].charCodeAt() - ((arrayCipher[0].charCodeAt()) - 65));
+        if (foo < 65) {
+          foo = foo + 26;
+        }
+        arrayResult.push(String.fromCharCode(foo));
+        arrayCipher.shift();
+      } else {
+        arrayResult.push(strUpper[i]);
+      }
+    }
+
+    if (this.condition) {
+      return arrayResult.join('');
+    } else {
+      return arrayResult.reverse().join('');
+    }
+  }
+
 }
 
 module.exports = {
